f73fa4d8b19a9b4aa62c0ec6fbc260004797fc34
xi-studio <110littletree@sina.com>
2022-05-10 14:13:22 +0800

    add train 20

--- src/dataset.py
+++ src/dataset.py
@@ -16,13 +16,19 @@ import h5py
 import csv
 import glob
 
-res = glob.glob("../data/AZ9010_256/*.png")
+radar_name = "../data/AZ9010_256_h5/Z_RADR_I_Z9010_%s_P_DOR_SA_R_10_230_15.010_clean.png"
+
+rain_list = []
+
+img_one_hot = np.eye(256)
+
+with open('train.csv') as f:
+    f_csv = csv.reader(f)
+    for row in f_csv:
+        rain_list.append(row)
 
 def default_loader(path):
     img = io.imread(path)
-    y = np.round(img/16.0)
-    y = torch.from_numpy(y[0])
-    y = y.type(torch.LongTensor)
 
     img = img/255.0
     img = torch.from_numpy(img)
@@ -31,16 +37,29 @@ def default_loader(path):
 
     return img, y 
 
+def predict_y(path):
+    img = io.imread(path)
+    y = np.round(img/16.0)
+    c = np.random.randint(256) 
+    y = torch.from_numpy(y[c])
+    y = y.type(torch.LongTensor)
+    c = img_one_hot[c]
+    c = torch.from_numpy(c)
+    
+    return y, c 
 
 class Radars(Dataset):
     def __init__(self,transform=None):
         super(Radars, self).__init__()
-        self.radar = res
+        self.radar = rain_list
 
     def __getitem__(self, index):
-        img, y = default_loader(self.radar[index])
+        img0 = default_loader(radar_name % self.radar[index][0])
+        img1 = default_loader(radar_name % self.radar[index][1])
+        y, c = predict_y(radar_name % self.radar[index][2])
+        img  = torch.cat((img0, img1), axis=1)
 
-        return img, y
+        return img, y, c
 
 
     def __len__(self):
